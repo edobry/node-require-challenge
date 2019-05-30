84db28a71e9a17eb459cc67adbfb6a560da79305
Eugene Dobry <edobry@gmail.com>
2019-05-29 23:56:23 -0400

    implemented parsing with precinct and (wrong) result flattening
    at this point i realized that someone else MUST have already done this work,
    went online to check, and realized that thee were several excellent packages,
    including one that did almost exactly what i needed.

--- index.js
+++ index.js
@@ -7,17 +7,27 @@ So, our goal here is to scan an arbitrary node.js project, on the local filesyst
 There are several points of distinction to be made here:
 1. "required modules" is a distinct concept from "dependencies"; we are not looking for packages referenced in config,
     or installed packages. this solutions must include only modules actually required in the project, including builtins
+
 2. there is some uncertainty in the spec regarding the output format; do we want a list of modules, with the files they're in,
     or rather a list of files, and the modules included in each? for many possible usecases, the latter seems more appropriate,
     but the wording implies the former, so I'll go with that until I get a chance to clarify this. its an easy change to make later on.
+
 3. there are MANY possible error modes for this project, such as the provided path not existing, the path being a file, the path not
     containing a node.js project, the files in the project being malformed javascript. the first few are pretty simple to guard for,
     but the last is slightly more complex. should we attempt to validate that each file is valid javascript? is that within scope?
     depending on the approach taken, the answer varies. if we decide to fully parse each file and work with the AST, it may be important.
     if we want to take the simpler, but more fragile approach of stringmatching requires, not so much.
+
+    UPDATE: going with AST parsing, as the work has already been done by someone else, thanks OS community
+
 4. the problem statement did not specify whether we're working with ES5, ES6, mjs, typescript, etc. one conclusion is that it may be prudent
     to handle all contingencies for the sake of completeness, but due to the added complexity of handling mjs, i'm going to opt for the path of
     only handling ES5/6 initially, and extending functionality later if need be, which is probably a better approach to take in a business setting
+
+    UPDATE: handling ES5 (commonjs, amd) and ES6 (modules) since the package i'm using supports that
+
+5. the problem statement also does not specify whether only the files directly in the folder should be considered, or also files in subfolders.
+    since i've already written the recursive descent, continuing under the assumption that the intent was all files in the tree
 */
 
 //first, lets gather what we need.
@@ -27,8 +37,8 @@ const
     //rampup time and allows me to focus on the core problem.
     nconf = require("nconf"),
 
-    //this package will actually do the brunt of the work
-    depCheck = require("dependency-check"),
+    //this package parses a file, produces the AST, and then scans that for various forms of `require`s
+    precinct = require("precinct"),
 
     //the builtin path module provides utilities for working with paths, which will come in useful
     path = require("path"),
@@ -89,13 +99,26 @@ const parseFolder = async dir => {
         return acc;
     }, { files: [], dirs: [] });
 
-    const fileVals = Promise.all(files.map(parseFile(dir)));
+    //process each file
+    const fileValPs = Promise.all(files.map(parseFile(dir)));
 
+    //process the subdirectories recursively
     const subDirs = await Promise.all(dirs.map(parseFolder));
+    const fileVals = await fileValPs;
+
+    console.log(subDirs);
+    console.log(fileVals);
+
+    //this is definitely wrong but just writing it for now
+    return [fileVals, ...subDirs];
 };
 
 const parseFile = dir => async name = {
     const contents = await fsP.readFile(`${dir}/${name}`, "utf8");
 
+    const deps = precinct(content);
 
+    //leaving room here to do any processing if needed
+
+    return deps;
 };
